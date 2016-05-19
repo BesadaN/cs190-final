e817be53cc257766d6de05279443b3d341a9b993
Besada Nakhla <besadanakhla@gmail.com>
2016-05-19 09:46:53 -0700

    Final's Playgrounds

--- PartIV_FactorialGenerator.playground/Contents.swift
+++ PartIV_FactorialGenerator.playground/Contents.swift
@@ -25,6 +25,15 @@ class FibonacciGenerator: GeneratorType {
  */
 class FactorialGenerator: GeneratorType {
     var state = (result: 1, multiplier: 1)
+    func next() ->Int? {
+        let incrementByOne = 1
+        let newValue = state.result * state.multiplier
+        let newMultiplier = state.multiplier + incrementByOne
+        state = (newValue, newMultiplier)
+        return state.result
+    }
+
+    
     // NOTE: When you are implementing this. Don't use somebody else's factorial function.
     // The whole point is to calculate each factorial result simply from the previous one.
     // E.g., if you have n! and you want (n+1)!, all your code has to do is multiple by n+1.
